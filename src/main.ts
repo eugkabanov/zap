6158f737e10c1882238e5b763ff1df38b5a745c9
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-09-17 21:24:29 -0500

    feat: add BalmUI framework

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import BalmUI from "balm-ui";
+import BalmUIPlus from "balm-ui-plus";
+
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import "./styles/index.scss";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(BalmUI);
+app.use(BalmUIPlus);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
+
+function resolve(dir: string) {
+  return path.join(__dirname, "..", "..", dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +16,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      vue: "vue/dist/vue.esm-bundler.js",
+      "balm-ui-plus": "balm-ui/dist/balm-ui-plus.esm.js",
+      "balm-ui-css": "balm-ui/dist/balm-ui.css",
+      "balm-ui-source": resolve("src/scripts"),
+      "balm-ui-plus-source": resolve("src/scripts/plus.js"),
     },
   },
 });
