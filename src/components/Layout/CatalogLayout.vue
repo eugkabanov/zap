669bbee22fe010839a8b94bd03b9f36cce7ffd7a
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-09-30 20:46:48 -0500

    feat(Catalog): add CatalogLayout component

---
+++ src/components/Layout/CatalogLayout.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isOpen = ref(false);
+</script>
+
+<template>
+  <div class="row">
+    <aside class="col-3">
+      <slot name="aside" />
+    </aside>
+    <main class="col grayed">
+      <div class="container-fluid">
+        <div class="mb-5">
+          <ui-icon-button class="d-xl-none" v-model="isOpen"
+            >menu_open</ui-icon-button
+          >
+        </div>
+        <slot name="main" />
+      </div>
+    </main>
+  </div>
+
+  <ui-drawer type="modal" v-model="isOpen">
+    <ui-drawer-content>
+      <!-- drawer needs at lease one focusable element -->
+      <div tabindex="1" />
+      <slot name="aside" />
+    </ui-drawer-content>
+  </ui-drawer>
+</template>
+
+<style scoped lang="scss">
+@use "@/styles/vars";
+
+.mdc-drawer--modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+
+aside {
+  padding: 50px 30px 50px 3rem;
+
+  @media (max-width: vars.$desktop) {
+    display: none;
+    padding: 25px 15px 25px 1.5rem;
+  }
+}
+</style>
--- src/views/Catalog/CatalogView.vue
+++ src/views/Catalog/CatalogView.vue
@@ -1,16 +1,23 @@
-<template>
-  <div class="row g-0">
-    <aside>
-      <h2 :class="$tt('headline2')">Каталог</h2>
-    </aside>
-    <main class="grayed">
-      <div></div>
-    </main>
-  </div>
-</template>
+<script setup lang="ts">
+import CatalogLayout from "../../components/Layout/CatalogLayout.vue";
+</script>
 
-<style scoped lang="scss">
-aside {
-  padding: 50px 80px 50ox 30px;
-}
-</style>
+<template>
+  <CatalogLayout>
+    <template #aside>
+      <div :class="$tt('headline2')" class="mb-5">Каталог</div>
+      <div>
+        <!-- aside content -->
+      </div>
+    </template>
+    <template #main>
+      <div class="mb-4">
+        <span :class="$tt('headline1')">Шины</span>
+        <span :class="$tt('body2')" class="ms-2 hint">250 226 товаров</span>
+      </div>
+      <div>
+        <!-- main content -->
+      </div>
+    </template>
+  </CatalogLayout>
+</template>
