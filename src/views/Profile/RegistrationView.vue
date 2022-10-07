8ab951c0df6623de5887e5eff18c05202ff504fc
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-10-07 16:38:23 -0500

    feat(registration): add organisation block

--- src/views/Profile/RegistrationView.vue
+++ src/views/Profile/RegistrationView.vue
@@ -37,7 +37,7 @@ const citiesList = [
       :options="registrationTypes"
     ></ui-chips>
 
-    <div class="mt-3 row gy-4">
+    <div v-if="registrationType === 0" class="mt-3 row gy-4">
       <div class="col-12 col-xl-3">
         <div class="large bold mb-5">Личные данные</div>
 
@@ -140,6 +140,76 @@ const citiesList = [
               alt="city map"
               class="city-map w-100"
             />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="registrationType === 1" class="mt-5">
+      <div class="large bold mb-5">Данные организации</div>
+
+      <div class="row gy-3">
+        <div class="col-12 col-md-6 col-xl-4">
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">ИНН</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Наименование</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Юридический адрес</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Фактический адрес</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2"
+              >Наименование банка</label
+            >
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-xl-4">
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">КПП</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Р/С</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">К/С</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">БИК</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">ОГРН</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-xl-4">
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">ОКПО</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Контактное лицо</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">E-mail</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
+          </div>
+          <div class="mb-3">
+            <label for="reg-name" class="d-block mb-2">Телефон</label>
+            <ui-textfield id="reg-name" outlined fullwidth />
           </div>
         </div>
       </div>
