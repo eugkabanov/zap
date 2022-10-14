a8e738eee4476c87e96633bb87460b6761b5e2c1
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-10-14 16:56:22 -0500

    feat(settings): add change password section

--- src/views/Profile/SettingsView.vue
+++ src/views/Profile/SettingsView.vue
@@ -136,7 +136,7 @@ const addressData = [
         </div>
       </section>
       <!-- Delivery details -->
-      <section v-if="selectedTab">
+      <section v-if="selectedTab === 2">
         <div class="large bold mb-4">Реквизиты</div>
 
         <ui-table
@@ -396,6 +396,35 @@ const addressData = [
             </div>
           </ui-dialog-content>
         </ui-dialog>
+      </section>
+      <!-- Password change -->
+      <section v-if="selectedTab === 3">
+        <div class="large bold mb-4">Изменить пароль</div>
+
+        <div class="col-12 col-xl-5">
+          <div class="mb-3 row align-items-center">
+            <label class="col-4">Старый пароль</label>
+            <div class="col-7 offset-1">
+              <ui-textfield input-type="password" outlined fullwidth />
+            </div>
+          </div>
+          <div class="mb-3 row align-items-center">
+            <label class="col-4">Новый пароль</label>
+            <div class="col-7 offset-1">
+              <ui-textfield input-type="password" outlined fullwidth />
+            </div>
+          </div>
+          <div class="mb-3 row align-items-center">
+            <label class="col-4">Повторите пароль</label>
+            <div class="col-7 offset-1">
+              <ui-textfield input-type="password" outlined fullwidth />
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-5">
+          <ui-button raised>Сохранить</ui-button>
+        </div>
       </section>
     </div>
   </main>
