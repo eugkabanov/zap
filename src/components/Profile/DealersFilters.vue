3117e25681bf0f5f9f9cf814144e44783ecd0d3d
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-10-15 22:52:48 -0500

    feat(dealers): add DealersFilters component

---
+++ src/components/Profile/DealersFilters.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bold large mb-5">Поиск</div>
+
+  <div class="mb-3">
+    <label class="d-block mb-2">Название</label>
+    <ui-textfield outlined fullwidth />
+  </div>
+  <div class="mb-3">
+    <label class="d-block mb-2">Регион</label>
+    <ui-select outlined fullwidth />
+  </div>
+  <div class="mb-3">
+    <ui-form-field class="mb-1">
+      <ui-checkbox input-id="filter-1" />
+      <label for="filter-1">Центральный склад</label>
+    </ui-form-field>
+    <ui-form-field class="mb-1">
+      <ui-checkbox input-id="filter-2" />
+      <label for="filter-2">Официальный дилер</label>
+    </ui-form-field>
+    <ui-form-field class="mb-1">
+      <ui-checkbox input-id="filter-3" />
+      <label for="filter-3">Остерегайтесь подделок</label>
+    </ui-form-field>
+    <ui-form-field class="mb-1">
+      <ui-checkbox input-id="filter-4" />
+      <label for="filter-4">Безвозвратный</label>
+    </ui-form-field>
+  </div>
+  <div class="mb-3">
+    <ui-button class="col-12" outlined>Показать отключенных</ui-button>
+  </div>
+
+  <h3 class="mt-5 mb-4">Настройки</h3>
+
+  <ui-form-field class="mb-1">
+    <ui-checkbox input-id="filter-5" />
+    <label for="filter-5">Автоматически отключать подделки</label>
+  </ui-form-field>
+  <ui-form-field class="mb-1">
+    <ui-checkbox input-id="filter-6" />
+    <label for="filter-6">Автоматически отключать безвозратные</label>
+  </ui-form-field>
+</template>
--- src/views/Profile/DealersView.vue
+++ src/views/Profile/DealersView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import DealersFilters from "../../components/Profile/DealersFilters.vue";
 
 const isFiltersOpen = ref(false);
 
@@ -35,8 +36,8 @@ const dealersData = [
     </h1>
 
     <div class="row">
-      <aside class="col-xl-3">
-        <!-- TODO: aside contents -->
+      <aside class="d-none d-xl-block col-xl-3">
+        <DealersFilters />
       </aside>
 
       <div class="offset-xl-1 col-12 col-xl-8 dark">
@@ -71,10 +72,10 @@ const dealersData = [
         </ui-table>
 
         <div class="mt-4 row justify-content-end">
-          <div class="col-auto">
+          <div class="col-6 col-md-auto">
             <ui-button outlined disabled>Включить (0)</ui-button>
           </div>
-          <div class="col-auto ms-2">
+          <div class="col-6 col-md-auto ms-md-2">
             <ui-button outlined disabled>Отключить (0)</ui-button>
           </div>
         </div>
@@ -83,8 +84,18 @@ const dealersData = [
   </div>
 
   <ui-drawer type="modal" v-model="isFiltersOpen">
-    <ui-drawer-content>
-      <!-- TODO: aside contents -->
+    <ui-drawer-content class="p-4">
+      <div tabindex="1" />
+
+      <DealersFilters />
     </ui-drawer-content>
   </ui-drawer>
 </template>
+
+<style scoped lang="scss">
+.mdc-drawer--modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+</style>
