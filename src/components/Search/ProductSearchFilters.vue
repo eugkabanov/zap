0eb22ceb25175d2d7e0ead2d353b3d1e832d5483
Evgenii Kabanov <eug.kabanov@icloud.com>
2022-10-16 18:16:55 -0500

    feat(producrSearch): add filters

---
+++ src/components/Search/ProductSearchFilters.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+defineProps<{
+  onFilterClick: () => unknown;
+}>();
+</script>
+
+<template>
+  <div class="row align-items-center mb-5">
+    <div class="col-auto">
+      <h2>Аналоги</h2>
+    </div>
+    <div class="col-auto hint">
+      <ui-form-field>
+        <ui-checkbox @click="onFilterClick" input-id="filter-all" />
+        <label for="filter-all">Показать все</label>
+      </ui-form-field>
+    </div>
+  </div>
+
+  <ui-tab-bar class="mb-4">
+    <ui-tab min-width content-indicator>По цене</ui-tab>
+    <ui-tab min-width content-indicator>По производителю</ui-tab>
+  </ui-tab-bar>
+
+  <div class="row align-items-center mb-2">
+    <div class="col-auto">
+      <ui-checkbox @click="onFilterClick" />
+    </div>
+    <div class="row col">
+      <div class="col-12 col-xl-auto">AMIVA</div>
+      <div class="d-none d-xl-block col dotted" />
+      <div class="col-auto">от 870 ₽</div>
+    </div>
+  </div>
+  <div class="row align-items-center mb-2">
+    <div class="col-auto">
+      <ui-checkbox @click="onFilterClick" />
+    </div>
+    <div class="row col">
+      <div class="col-12 col-xl-auto">MARKON</div>
+      <div class="d-none d-xl-block col dotted" />
+      <div class="col-auto">от 927 ₽</div>
+    </div>
+  </div>
+  <div class="row align-items-center mb-2">
+    <div class="col-auto">
+      <ui-checkbox @click="onFilterClick" />
+    </div>
+    <div class="row col">
+      <div class="col-12 col-xl-auto">QUATTRO FRENI</div>
+      <div class="d-none d-xl-block col dotted" />
+      <div class="col-auto">от 1154 ₽</div>
+    </div>
+  </div>
+  <div class="row align-items-center mb-2">
+    <div class="col-auto">
+      <ui-checkbox @click="onFilterClick" />
+    </div>
+    <div class="row col">
+      <div class="col-12 col-xl-auto">CWORKS</div>
+      <div class="d-none d-xl-block col dotted" />
+      <div class="col-auto">от 1196 ₽</div>
+    </div>
+  </div>
+</template>
--- src/views/ProductSearch.vue
+++ src/views/ProductSearch.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import ProductSearchFilters from "../components/Search/ProductSearchFilters.vue";
+
+const isSearchFiltersOpen = ref(false);
+const isSimilarShow = ref(false);
+
+const onFilterClick = () => (isSimilarShow.value = true);
+
 const searchData = [
   {
     brand: "NOKIAN",
@@ -40,83 +48,106 @@ const similarSearchData = [
 <template>
   <main class="py-5 container-fluid">
     <h1 class="mb-5">
-      Артикул TS32701
+      <ui-icon-button class="d-xl-none" v-model="isSearchFiltersOpen"
+        >menu_open</ui-icon-button
+      >Артикул TS32701
       <span :class="$tt('body1')" class="ms-3 hint">найдено 2 товара</span>
     </h1>
 
-    <div class="mb-5">
-      <ui-table
-        fullwidth
-        :data="searchData"
-        :thead="[
-          { value: 'Бренд' },
-          { value: 'Поставщик' },
-          { value: 'Количество' },
-          { value: 'Срок' },
-          { value: 'Цена' },
-          { value: 'Отправка поставщику' },
-          { value: '' },
-          { value: '' },
-        ]"
-        :tbody="[
-          { field: 'brand' },
-          { field: 'dealer' },
-          { field: 'count' },
-          { field: 'deadline' },
-          { field: 'price' },
-          { field: 'date' },
-          { slot: 'cart' },
-          { slot: 'delivery' },
-        ]"
-      >
-        <template #cart>
-          <ui-icon class="hint" outlined> shopping_cart </ui-icon>
-        </template>
-        <template #delivery>
-          <ui-icon class="hint" outlined> directions_car </ui-icon>
-        </template>
-      </ui-table>
-    </div>
+    <div class="row">
+      <div class="col-12 col-xl-8">
+        <div class="mb-5">
+          <ui-table
+            fullwidth
+            :data="searchData"
+            :thead="[
+              { value: 'Бренд' },
+              { value: 'Поставщик' },
+              { value: 'Количество' },
+              { value: 'Срок' },
+              { value: 'Цена' },
+              { value: 'Отправка поставщику' },
+              { value: '' },
+              { value: '' },
+            ]"
+            :tbody="[
+              { field: 'brand' },
+              { field: 'dealer' },
+              { field: 'count' },
+              { field: 'deadline' },
+              { field: 'price' },
+              { field: 'date' },
+              { slot: 'cart' },
+              { slot: 'delivery' },
+            ]"
+          >
+            <template #cart>
+              <ui-icon class="hint" outlined> shopping_cart </ui-icon>
+            </template>
+            <template #delivery>
+              <ui-icon class="hint" outlined> directions_car </ui-icon>
+            </template>
+          </ui-table>
+        </div>
+        <div v-if="isSimilarShow">
+          <h2 class="mb-5">Аналоги других производителей</h2>
+          <ui-tab-bar class="mb-4">
+            <ui-tab min-width content-indicator>По цене</ui-tab>
+            <ui-tab min-width content-indicator>По производителю</ui-tab>
+          </ui-tab-bar>
+          <ui-table
+            fullwidth
+            :data="similarSearchData"
+            :thead="[
+              { value: 'Бренд' },
+              { value: 'Поставщик' },
+              { value: 'Количество' },
+              { value: 'Срок' },
+              { value: 'Цена' },
+              { value: 'Отправка поставщику' },
+              { value: '' },
+              { value: '' },
+            ]"
+            :tbody="[
+              { field: 'brand' },
+              { field: 'dealer' },
+              { field: 'count' },
+              { field: 'deadline' },
+              { field: 'price' },
+              { field: 'date' },
+              { slot: 'cart' },
+              { slot: 'delivery' },
+            ]"
+          >
+            <template #cart>
+              <ui-icon class="hint" outlined> shopping_cart </ui-icon>
+            </template>
+            <template #delivery>
+              <ui-icon class="hint" outlined> directions_car </ui-icon>
+            </template>
+          </ui-table>
+        </div>
+      </div>
 
-    <div>
-      <h2 class="mb-5">Аналоги других производителей</h2>
-
-      <ui-tab-bar class="mb-4">
-        <ui-tab min-width content-indicator>По цене</ui-tab>
-        <ui-tab min-width content-indicator>По производителю</ui-tab>
-      </ui-tab-bar>
-
-      <ui-table
-        fullwidth
-        :data="similarSearchData"
-        :thead="[
-          { value: 'Бренд' },
-          { value: 'Поставщик' },
-          { value: 'Количество' },
-          { value: 'Срок' },
-          { value: 'Цена' },
-          { value: 'Отправка поставщику' },
-          { value: '' },
-          { value: '' },
-        ]"
-        :tbody="[
-          { field: 'brand' },
-          { field: 'dealer' },
-          { field: 'count' },
-          { field: 'deadline' },
-          { field: 'price' },
-          { field: 'date' },
-          { slot: 'cart' },
-          { slot: 'delivery' },
-        ]"
-      >
-        <template #cart>
-          <ui-icon class="hint" outlined> shopping_cart </ui-icon>
-        </template>
-        <template #delivery>
-          <ui-icon class="hint" outlined> directions_car </ui-icon>
-        </template>
-      </ui-table>
+      <div class="d-none d-xl-block offset-1 col-3">
+        <ProductSearchFilters @filter-click="onFilterClick" />
+      </div>
     </div>
   </main>
+
+  <ui-drawer type="modal" v-model="isSearchFiltersOpen">
+    <ui-drawer-content class="p-4">
+      <!-- drawer needs at lease one focusable element -->
+      <div tabindex="1" />
+      <ProductSearchFilters @filter-click="onFilterClick" />
+    </ui-drawer-content>
+  </ui-drawer>
 </template>
+
+<style scoped lang="scss">
+.mdc-drawer--modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+</style>
