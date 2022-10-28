18639050cc60e0182851ee3dee7d480267d461ef
Alexander <avkorovushkin@gmail.com>
2022-10-28 16:37:05 +0300

    chooseBrandView

--- src/services/CatalogService.ts
+++ src/services/CatalogService.ts
@@ -1,9 +1,19 @@
+import type CatalogWizard2Object from "@/types/CatalogWizard2Object";
 import HTTPClient from "../http-common/client_http";
 
 class CatalogService {
 
   listCatalogs(): Promise<any> {
-    return HTTPClient.get("/listCatalogs");
+    return HTTPClient.get("/catalog/listCatalogs");
+  }
+
+  catalogWizard2(catalog : string): Promise<any> {
+    console.log("catalog")
+    console.log(catalog)
+    let params = {
+      'catalog': catalog
+    };
+    return HTTPClient.get<CatalogWizard2Object[]>("/catalog/catalogWizard2",  { params : params });
   }
 
 }
