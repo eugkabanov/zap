9820be36403e782df983aad263c899c832e4e0d5
yakovlev-aa <yakovalex11@gmail.com>
2022-12-19 12:50:20 +0300

    Совместная работа фильтров на странице "Заказы".

--- src/views/Profile/OrdersView.vue
+++ src/views/Profile/OrdersView.vue
@@ -315,9 +315,18 @@ export default defineComponent({
       this.items = this.itemsTech
       this.textSearchBar = ''
       if (this.selectedValueVendorCode != "Non") {
-        this.items = this.items.filter(item => item.vendorCode == vendorCode)
+        if (this.selectedValueStatus != "Non" && this.selectedValueStatus != "") {
+          this.items = this.items.filter(item => item.status == this.selectedValueStatus)
+          this.items = this.items.filter(item => item.vendorCode == vendorCode)
+        } else {
+          this.items = this.items.filter(item => item.vendorCode == vendorCode)
+        }
       } else {
-        this.items = this.itemsTech
+        if (this.selectedValueStatus != "Non" && this.selectedValueStatus != "") {
+          this.items = this.items.filter(item => item.status == this.selectedValueStatus  )
+        } else {
+          this.items = this.itemsTech
+        }
       }
     },
 
@@ -325,9 +334,18 @@ export default defineComponent({
       this.items = this.itemsTech
       this.textSearchBar = ''
       if (this.selectedValueStatus != "Non") {
-        this.items = this.items.filter(item => item.status == status)
+        if (this.selectedValueVendorCode != "Non" && this.selectedValueVendorCode != "") {
+          this.items = this.items.filter(item => item.vendorCode == this.selectedValueVendorCode)
+          this.items = this.items.filter(item => item.status == status)
+        } else {
+          this.items = this.items.filter(item => item.status == status)
+        }
       } else {
-        this.items = this.itemsTech
+        if (this.selectedValueVendorCode != "Non" && this.selectedValueVendorCode != "") {
+          this.items = this.items.filter(item => item.vendorCode == this.selectedValueVendorCode)
+        } else {
+          this.items = this.itemsTech
+        }
       }
     }
   },
